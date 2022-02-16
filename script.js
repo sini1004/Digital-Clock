3015de9df091e4c92a99fa5926d419d67b408a5b
shinhee <tlsgml656@gmail.com>
2022-02-16 21:10:48 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+let handleId = 0;
+const h1 = document.getElementById("time")
+const go = document.getElementById("go")
+const stop = document.getElementById("stop")
+
+function getTime(){
+    const date = new Date();
+    const hour = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    const time = `${hour} : ${minutes} : ${seconds}`
+
+    h1.textContent = time;
+}
+
+go.onclick = function(){
+    if(handleId == 0){
+        handleId = setInterval(getTime, 1000) //interval은 0이 아닌 정수를 내보내는 특징.
+    }
+}
+
+stop.onclick = function(){
+    clearInterval(handleId)
+    handleId = 0;
+}
+
